1c8e8615f1a6232b95f8b0124d3ef0f8bf32a92f
Adriel Alves Ferreira de Souza <adriel.a2006@aluno.ifsc.edu.br>
2025-05-27 01:00:21 +0000

    teste da base do catalogo

--- app/src/test/java/ifsc/poo/AppTest.java
+++ app/src/test/java/ifsc/poo/AppTest.java
@@ -3,7 +3,85 @@
  */
 package ifsc.poo;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import java.time.LocalDate;
+import java.util.*;
 
-public class AppTest {}
+class Filme {
+    private String titulo;
+    private String genero;
+    private LocalDate lancamento;
+
+    public Filme(String titulo, String genero, LocalDate lancamento) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.lancamento = lancamento;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public LocalDate lancamento() {
+        return lancamento;
+    }
+ 
+    
+    public String toString() {
+        return String.format("Filme{titulo='%s', genero='%s', lancamento=%s}", titulo, genero, lancamento);
+    }
+}
+
+class Catalogo {
+    private List<Filme> filmes;
+
+    public Catalogo() {
+        this.filmes = new ArrayList<>();
+    }
+
+    public void adicionarFilme(Filme filme) {
+        filmes.add(filme);
+    }
+
+    public void ordemTitulo() {
+        filmes.sort(Comparator.comparing(Filme::getTitulo));
+    }
+
+    public void ordemGenero() {
+        filmes.sort(Comparator.comparing(Filme::getGenero));
+    }
+
+    public void ordemLancamento() {
+        filmes.sort(Comparator.comparing(Filme::getDataLancamento));
+    }
+
+    public void mostrarCatalogo() {
+        filmes.forEach(System.out::println);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Catalogo catalogo = new Catalogo();
+
+        catalogo.adicionarFilme(new Filme("O Poderoso Chefão", "Crime", LocalDate.of(1972, 3, 24)));
+        catalogo.adicionarFilme(new Filme("O Senhor dos Anéis: A Sociedade do Anel", "Fantasia", LocalDate.of(2001, 12, 19)));
+        catalogo.adicionarFilme(new Filme("Pulp Fiction", "Crime", LocalDate.of(1994, 10, 14)));
+        catalogo.adicionarFilme(new Filme("Forrest Gump", "Drama", LocalDate.of(1994, 7, 6)));
+
+        System.out.println("Ordenados por título:");
+        catalogo.ordenarPorTitulo();
+        catalogo.exibirCatalogo();
+
+        System.out.println("\nOrdenados por gênero:");
+        catalogo.ordenarPorGenero();
+        catalogo.exibirCatalogo();
+
+        System.out.println("\nOrdenados por data de lançamento:");
+        catalogo.ordenarPorDataLancamento();
+        catalogo.exibirCatalogo();
+    }
+}
